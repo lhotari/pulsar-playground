ddb46869bd800ce30d0f433be635059234c66391
Lari Hotari <lari@hotari.net>
2021-03-31 11:49:44 +0300

    Add support for calling pulsar-admin in cluster-b

--- test-env/multi_cluster_pulsar_admin.sh
+++ test-env/multi_cluster_pulsar_admin.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -xe
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-
+CLUSTER_NAME=cluster-a
+if [[ "$1" == "--cluster-b" ]]; then
+  CLUSTER_NAME=cluster-b
+  shift
+fi
 function pulsar_admin() {
-  kubectl exec -i -t -n cluster-a cluster-a-pulsar-broker-0 -- \
+  kubectl exec -i -t -n $CLUSTER_NAME $CLUSTER_NAME-pulsar-broker-0 -- \
    bin/pulsar-admin "${@}"
 }
 
