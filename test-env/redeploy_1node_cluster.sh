a8796e4eea0b2cfa15f8c2058c7566feb0ede6f2
Lari Hotari <lari@hotari.net>
2021-04-13 19:28:19 +0300

    Make it possible to pass custom images file

--- test-env/redeploy_1node_cluster.sh
+++ test-env/redeploy_1node_cluster.sh
@@ -5,4 +5,6 @@ helm uninstall pulsar -n pulsar && { echo 'Waiting 10 seconds...'; sleep 10; }
 echo 'Removing pulsar namespace'
 kubectl delete namespaces/pulsar
 echo 'Installing 1node cluster'
-helm upgrade --install --namespace pulsar --create-namespace -f $SCRIPT_DIR/1node/values.yaml -f $SCRIPT_DIR/custom_images.yaml --set initialize=true pulsar apache/pulsar
+custom_images=${1:-$SCRIPT_DIR/custom_images.yaml}
+echo "Using custom images from ${custom_images} file"
+helm upgrade --install --namespace pulsar --create-namespace -f $SCRIPT_DIR/1node/values.yaml -f $custom_images --set initialize=true pulsar apache/pulsar
