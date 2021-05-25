61dbc05d1da021f12c75beed419c060e9f56559d
Lari Hotari <lari@hotari.net>
2021-05-25 10:52:41 +0300

    Fix log output

--- test-env/delete_deployment.sh
+++ test-env/delete_deployment.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 $SCRIPT_DIR/check_kubectx.sh || exit 1
 echo 'Uninstalling old installation'
 helm uninstall "${DEPLOYMENT_NAME}" -n "${DEPLOYMENT_NAMESPACE}" && { echo 'Waiting 10 seconds...'; sleep 10; }
-echo 'Removing "${DEPLOYMENT_NAMESPACE}" namespace'
+echo "Removing '${DEPLOYMENT_NAMESPACE}' namespace"
 kubectl delete namespaces/"${DEPLOYMENT_NAMESPACE}" --grace-period=0 --force
 exit 0
