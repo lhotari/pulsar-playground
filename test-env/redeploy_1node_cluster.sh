dfce2b73f87f4d5ad0687728821461ce567c76b6
Lari Hotari <lari@hotari.net>
2021-06-11 09:18:33 +0300

    Support multiple values files in script

--- test-env/redeploy_1node_cluster.sh
+++ test-env/redeploy_1node_cluster.sh
@@ -3,6 +3,16 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 $SCRIPT_DIR/delete_deployment.sh || exit 1
 . $SCRIPT_DIR/test-env.env
 echo 'Installing 1node cluster'
-IMAGES_YAML=${1:-$SCRIPT_DIR/java_test_images.yaml}
-echo "Using images from ${IMAGES_YAML} file"
-helm upgrade --install --namespace "${DEPLOYMENT_NAMESPACE}" --create-namespace -f $SCRIPT_DIR/1node/values.yaml -f $IMAGES_YAML --set initialize=true "${DEPLOYMENT_NAME}" apache/pulsar
+value_files=""
+for arg in "$@"; do
+    if [ -f "$arg" ]; then
+        value_files+=" -f $arg"
+    else
+        >&2 echo "Cannot find $arg" && exit 1
+    fi
+done
+if [ -z "$value_files" ]; then
+    value_files="-f $SCRIPT_DIR/java_test_images.yaml"
+fi
+echo "Using values '${value_files}'"
+helm upgrade --install --namespace "${DEPLOYMENT_NAMESPACE}" --create-namespace -f $SCRIPT_DIR/1node/values.yaml $value_files --set initialize=true "${DEPLOYMENT_NAME}" apache/pulsar
