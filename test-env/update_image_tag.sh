7c396810901617dc86c003a952c083a13069e6f0
Lari Hotari <lari@hotari.net>
2021-12-22 12:36:32 +0200

    Combine java_test_images keys in a single file for both Helm chart types (Apache & DS)

--- test-env/update_image_tag.sh
+++ test-env/update_image_tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # requires yq, https://mikefarah.gitbook.io/yq/
 # https://mikefarah.gitbook.io/yq/operators/assign-update#updated-multiple-paths
-image_tag="$1" yq eval -i '(.images.zookeeper.tag, .images.bookie.tag, .images.autorecovery.tag, .images.broker.tag, .images.proxy.tag, .images.functions.tag, .pulsar_metadata.image.tag) |= env(image_tag)' $2
+image_tag="$1" yq eval -i '(.images.zookeeper.tag, .images.bookie.tag, .images.autorecovery.tag, .images.broker.tag, .images.proxy.tag, .images.functions.tag, .pulsar_metadata.image.tag, .image.broker.tag, .image.brokerSts.tag, .image.function.tag, .image.zookeeper.tag, .image.bookkeeper.tag, .image.proxy.tag) |= env(image_tag)' $2
 
--- test-env/update_java_test_image_tag.sh
+++ test-env/update_java_test_image_tag.sh
@@ -3,4 +3,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # requires yq, https://mikefarah.gitbook.io/yq/
 # https://mikefarah.gitbook.io/yq/operators/assign-update#updated-multiple-paths
 cd $SCRIPT_DIR
-./update_image_tag.sh "${1:-$(docker images localhost:32000/apachepulsar/java-test-image --format '{{ .Tag }}' | head -n 1)}" java_test_images.yaml
+tag="${1:-$(docker images localhost:32000/apachepulsar/java-test-image --format '{{ .Tag }}' | head -n 1)}"
+./update_image_tag.sh "$tag" java_test_images.yaml
