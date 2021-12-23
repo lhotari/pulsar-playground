8096b3819b842f0356bfd6525f7771a220de30f0
Lari Hotari <lari@hotari.net>
2021-12-23 03:19:30 +0200

    Use TLS in scripts

--- test-env/georep_consume_in_cluster_b.sh
+++ test-env/georep_consume_in_cluster_b.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-pulsar-perf consume --service-url pulsar://cluster-b-pulsar-proxy.cluster-b.svc.cluster.local:6650 "$@" persistent://georep/replicated/perftest
+pulsar-perf consume \
+    --service-url pulsar+ssl://cluster-b-pulsar-proxy.cluster-b.svc.cluster.local:6651 \
+    --tls-allow-insecure \
+    --auth_plugin org.apache.pulsar.client.impl.auth.AuthenticationToken \
+    --auth-params "{token: \"$(kubectl exec -n cluster-b pod/cluster-b-pulsar-broker-0 -c cluster-b-pulsar-broker -- cat /pulsar/token-superuser-stripped.jwt)\"}" \
+    "$@" persistent://georep/replicated/perftest
--- test-env/georep_produce_in_cluster_a.sh
+++ test-env/georep_produce_in_cluster_a.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-pulsar-perf produce --service-url pulsar://cluster-a-pulsar-proxy.cluster-a.svc.cluster.local:6650 "$@" persistent://georep/replicated/perftest
+pulsar-perf produce \
+    --service-url pulsar+ssl://cluster-a-pulsar-proxy.cluster-a.svc.cluster.local:6651 \
+    --tls-allow-insecure \
+    --auth_plugin org.apache.pulsar.client.impl.auth.AuthenticationToken \
+    --auth-params "{token: \"$(kubectl exec -n cluster-a pod/cluster-a-pulsar-broker-0 -c cluster-a-pulsar-broker -- cat /pulsar/token-superuser-stripped.jwt)\"}" \
+    "$@" persistent://georep/replicated/perftest
+
