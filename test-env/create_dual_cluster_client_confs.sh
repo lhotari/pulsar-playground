88cc459c96e8d91776c572611baed578f394e1b4
Lari Hotari <lari@hotari.net>
2022-01-13 16:03:04 +0200

    Add script for creating client conf files for clusters

---
+++ test-env/create_dual_cluster_client_confs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null
+
+function set_current_cluster() {
+    current_namespace="$1"
+}
+
+function run_command_in_cluster() {
+    local admin_script="$1"
+    kubectl exec -i -n "$current_namespace" "$(kubectl get pod -n "$current_namespace" -l component=broker -o name | head -1)" -c ${current_namespace}-pulsar-broker -- bash -c "export PATH=/pulsar/bin:\$PATH; ${admin_script}"
+}
+
+function create_client_conf_for_cluster() {
+    local cid=$1
+    set_current_cluster "cluster-${cid}"
+    token="$(run_command_in_cluster "cat /pulsar/token-superuser-stripped.jwt")"
+    cat > client_cluster-${cid}.conf <<EOF
+webServiceUrl=https://cluster-${cid}-pulsar-proxy.cluster-${cid}.svc.cluster.local:8443
+brokerServiceUrl=pulsar+ssl://cluster-${cid}-pulsar-proxy.cluster-${cid}.svc.cluster.local:6651
+authPlugin=org.apache.pulsar.client.impl.auth.AuthenticationToken
+authParams=token:${token}
+tlsAllowInsecureConnection=true
+EOF
+}
+
+create_client_conf_for_cluster a
+create_client_conf_for_cluster b
