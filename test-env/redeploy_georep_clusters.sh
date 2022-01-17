34dea37fea931567104d24d31ba0f8e613b4674e
Lari Hotari <lari@hotari.net>
2022-01-17 18:22:01 +0200

    Support passing additional arguments
    to support deploying with java test image
    ./redeploy_georep_clusters.sh java_test_images.yaml

--- test-env/redeploy_georep_clusters.sh
+++ test-env/redeploy_georep_clusters.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CHART=datastax-pulsar/pulsar ./redeploy_dual_cluster.sh datastax_tls_and_token_auth.yaml --set brokerSts.replicaCount=3
+CHART=datastax-pulsar/pulsar ./redeploy_dual_cluster.sh datastax_tls_and_token_auth.yaml "$@" --set brokerSts.replicaCount=3
