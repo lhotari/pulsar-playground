f1a0fc26eb3cdf2f6e54a278674114ba23f3972e
Lari Hotari <lari@hotari.net>
2022-01-18 11:27:57 +0200

    Use static token, not read from file

--- test-env/configure_georep.sh
+++ test-env/configure_georep.sh
@@ -99,7 +99,8 @@ function set_up_georep (){
     read -r -d '' admin_script <<EOF
 pulsar-admin tenants create ${georep_tenant}
 pulsar-admin namespaces create -b 64 ${georep_namespace}
-pulsar-admin clusters create --tls-enable --tls-allow-insecure --tls-trust-certs-filepath /pulsar/certs/tls.crt --auth-plugin org.apache.pulsar.client.impl.auth.AuthenticationToken --auth-parameters file:///pulsar/token-superuser-stripped.jwt --broker-url-secure pulsar+ssl://${peer_cluster_dns}:6651 --url-secure https://${peer_cluster_dns}:8443 ${peer_cluster_name}
+token="\$(cat /pulsar/token-superuser/superuser.jwt)"
+pulsar-admin clusters create --tls-enable --tls-allow-insecure --tls-trust-certs-filepath /pulsar/certs/tls.crt --auth-plugin org.apache.pulsar.client.impl.auth.AuthenticationToken --auth-parameters "token:\${token}" --broker-url-secure pulsar+ssl://${peer_cluster_dns}:6651 --url-secure https://${peer_cluster_dns}:8443 ${peer_cluster_name}
 #pulsar-admin clusters create --broker-url pulsar://${peer_cluster_dns}:6650 --url http://${peer_cluster_dns}:8080 ${peer_cluster_name}
 pulsar-admin tenants update --allowed-clusters "${cluster_a_id},${cluster_b_id}" ${georep_tenant}
 pulsar-admin namespaces set-clusters -c ${own_cluster_name},${peer_cluster_name} ${georep_namespace}
