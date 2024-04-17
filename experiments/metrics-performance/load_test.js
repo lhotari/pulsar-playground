1c4b1102b7a6b95f433cef075d153a9826c55c8e
Lari Hotari <lari@hotari.net>
2024-04-17 19:09:29 +0300

    Set "discardResponseBodies: true" also for load_test.js
    - https://k6.io/docs/using-k6/k6-options/reference/#discard-response-bodies

--- experiments/metrics-performance/load_test.js
+++ experiments/metrics-performance/load_test.js
@@ -2,6 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
+    discardResponseBodies: true,
     vus: 100, // Number of virtual users
     iterations: 10000, // Total number of request iterations
 };
