47b558cb83f1e9252206fe18694c0a0dce36e57d
Lari Hotari <lhotari@users.noreply.github.com>
2024-05-21 23:02:36 +0300

    Update directory where the patched pulsar-perf is installed

--- issues/issue22601/common.sh
+++ issues/issue22601/common.sh
@@ -1,2 +1,2 @@
 export PULSAR_CLIENT_CONF=$HOME/workspace-pulsar/pulsar-playground/test-env/client_tls_broker.conf
-export PATH="$HOME/workspace-pulsar/pulsar/bin:$PATH"
+export PATH="$HOME/workspace-pulsar/pulsar-debugging/apache-pulsar-3.2.3-patched-pulsar-perf/bin:$PATH"
