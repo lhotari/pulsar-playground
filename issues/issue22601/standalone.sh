1aeb1323b709c7b8852b3362a8438472c422e88f
Lari Hotari <lari@hotari.net>
2024-05-22 07:12:26 +0300

    Simulate 8 cores

--- issues/issue22601/standalone.sh
+++ issues/issue22601/standalone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PULSAR_STANDALONE_USE_ZOOKEEPER=1 bin/pulsar standalone -c $SCRIPT_DIR/standalone_tls.conf --wipe-data --num-bookies 3 -nss -nfw 2>&1 | tee standalone.log
+OPTS="-XX:ActiveProcessorCount=8" PULSAR_STANDALONE_USE_ZOOKEEPER=1 bin/pulsar standalone -c $SCRIPT_DIR/standalone_tls.conf --wipe-data --num-bookies 3 -nss -nfw 2>&1 | tee standalone.log
