316fe26bd70bf448bff28b4c36d02d6040969827
Lari Hotari <lari@hotari.net>
2024-05-28 19:11:23 +0300

    Use patched 3.2.2 perf-client

--- issues/issue22601/standalone_env/common_perf_client.sh
+++ issues/issue22601/standalone_env/common_perf_client.sh
@@ -1,3 +1,3 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export PULSAR_HOME="$SCRIPT_DIR/apache-pulsar-3.2.3"
+export PULSAR_HOME="$HOME/workspace-pulsar/pulsar-debugging/apache-pulsar-3.2.2-patched-pulsar-perf"
 export PATH="$PULSAR_HOME/bin:$PATH"
