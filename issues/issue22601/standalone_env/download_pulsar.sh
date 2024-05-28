67536413397c4448d013da47f534fbe090b8641b
Lari Hotari <lari@hotari.net>
2024-05-28 19:04:36 +0300

    Download Pulsar 3.2.3

--- issues/issue22601/standalone_env/download_pulsar.sh
+++ issues/issue22601/standalone_env/download_pulsar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://archive.apache.org/dist/pulsar/pulsar-3.2.2/apache-pulsar-3.2.2-bin.tar.gz | tar zxvf -
+curl https://archive.apache.org/dist/pulsar/pulsar-3.2.3/apache-pulsar-3.2.3-bin.tar.gz | tar zxvf -
