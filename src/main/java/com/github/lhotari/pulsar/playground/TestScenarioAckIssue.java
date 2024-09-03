e183b6bd6b874c9c396ce7cd8cff0a9cb5482845
Lari Hotari <lhotari@users.noreply.github.com>
2024-09-03 16:24:52 +0300

    Remove scheduled and unordered acks

--- src/main/java/com/github/lhotari/pulsar/playground/TestScenarioAckIssue.java
+++ src/main/java/com/github/lhotari/pulsar/playground/TestScenarioAckIssue.java
@@ -282,26 +282,22 @@ public class TestScenarioAckIssue {
                         duplicates);
                 ackSent.incrementAndGet();
                 if (ackAsync) {
-                    executorService.schedule(() -> {
-                        CompletableFuture.runAsync(() -> {
-                            // increase chances for race condition
-                            waitForOtherThreadsToTestRaceConditions(ackPhaser);
-                            try {
-                                consumer.acknowledgeAsync(msg).handle((result, throwable) -> {
-                                    if (throwable == null) {
-                                        ackSuccess.incrementAndGet();
-                                    } else {
-                                        log.error("Failed to acknowledge message", throwable);
-                                        ackFailed.incrementAndGet();
-                                    }
-                                    return null;
-                                });
-                            } catch (Throwable t) {
-                                log.error("Failed to acknowledge message", t);
+                    // increase chances for race condition
+                    waitForOtherThreadsToTestRaceConditions(ackPhaser);
+                    try {
+                        consumer.acknowledgeAsync(msg).handle((result, throwable) -> {
+                            if (throwable == null) {
+                                ackSuccess.incrementAndGet();
+                            } else {
+                                log.error("Failed to acknowledge message", throwable);
                                 ackFailed.incrementAndGet();
                             }
+                            return null;
                         });
-                    }, random.nextInt(100) + 1, TimeUnit.MILLISECONDS);
+                    } catch (Throwable t) {
+                        log.error("Failed to acknowledge message", t);
+                        ackFailed.incrementAndGet();
+                    }
                 } else {
                     consumer.acknowledge(msg);
                     ackSuccess.incrementAndGet();
