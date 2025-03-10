d90ec90e99158f5f3a6176bc643d8799a4d7a505
Lari Hotari <lari@hotari.net>
2025-03-10 22:41:12 +0200

    Update install_chart.sh

--- pulsar-helm-chart-dev/install_chart.sh
+++ pulsar-helm-chart-dev/install_chart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CHART_HOME="${SCRIPT_DIR}"/../../pulsar-helm-chart
+helm dependency update "${CHART_HOME}"/charts/pulsar
 # installs or upgrades chart
 helm upgrade --install --namespace pulsar --create-namespace \
-  pulsar charts/pulsar \
-  --values "${SCRIPT_DIR}"/../../pulsar-helm-chart/examples/values-testing.yaml \
+  pulsar "${CHART_HOME}"/charts/pulsar \
+  --values "${CHART_HOME}"/examples/values-testing.yaml \
   "$@"
