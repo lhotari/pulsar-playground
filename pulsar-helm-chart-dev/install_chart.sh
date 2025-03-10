050931678ba5763f975e8681ad1f566989f95747
Lari Hotari <lari@hotari.net>
2025-03-10 23:46:05 +0200

    Allow skipping update

--- pulsar-helm-chart-dev/install_chart.sh
+++ pulsar-helm-chart-dev/install_chart.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -xe
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CHART_HOME="${SCRIPT_DIR}"/../../pulsar-helm-chart
-helm dependency update "${CHART_HOME}"/charts/pulsar
+if [[ "$1" == "--skip-dependency-update" ]]; then
+  no_update=1
+  shift
+fi
+if [[ "$no_update" != "1" ]]; then
+    helm dependency update "${CHART_HOME}"/charts/pulsar
+fi
 # installs or upgrades chart
 helm upgrade --install --namespace pulsar --create-namespace \
   pulsar "${CHART_HOME}"/charts/pulsar \
