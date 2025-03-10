957e27be2ba670750815de175cd7dfa34fed87c8
Lari Hotari <lari@hotari.net>
2025-03-11 01:01:24 +0200

    Add reconfiguring of dns

--- microk8s/start_microk8s.sh
+++ microk8s/start_microk8s.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo snap enable microk8s
 sudo microk8s start
+echo "Waiting 5 seconds"
+sleep 5
+echo "Reconfiguring dns"
+"$SCRIPT_DIR"/reconfigure_dns.sh
--- microk8s/stop_microk8s.sh
+++ microk8s/stop_microk8s.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sudo rm /etc/systemd/resolved.conf.d/00-k8s-dns-resolver.conf
+sudo systemctl restart systemd-resolved
 sudo microk8s stop
 sudo snap disable microk8s
